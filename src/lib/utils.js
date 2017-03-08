7027fb78fbcabf4b637126b9031decc49b66c3ad
Fabio Albertin <albertin@gmail.com>
2017-03-08 00:14:47 +0000

    Finished part 14

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,2 +1,6 @@
 // ...s : make arr, spread arr
 export const partial = (fn, ...args) => fn.bind(null, ...args);
+
+const basePipe = (fn1, fn2) => (...args) => fn2(fn1(...args));
+
+export const pipe = (...fnN) => fnN.reduce(basePipe);
