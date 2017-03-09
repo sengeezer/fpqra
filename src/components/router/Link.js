81c08f4f3c3e274d9e385a807c287302032e722e
Fabio Albertin <albertin@gmail.com>
2017-03-09 01:34:43 +0000

    Finished part 17

--- src/components/router/Link.js
+++ src/components/router/Link.js
@@ -1,14 +1,20 @@
 import React, {Component} from 'react';
 
 export class Link extends Component {
+  static contextTypes = {
+    route: React.PropTypes.string,
+    linkHandler: React.PropTypes.func
+  };
+
   handleClick = (evt) => {
     evt.preventDefault();
-    history.pushState(null, '', this.props.to);
-  }
+    this.context.linkHandler(this.props.to);
+  };
 
   render() {
+    const activeClass = this.context.route === this.props.to ? 'active' : '';
     return (
-      <a href="#" onClick={this.handleClick}>{this.props.children}</a>
+      <a href="#" className={activeClass} onClick={this.handleClick}>{this.props.children}</a>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {Router} from './components/router';
 import './index.css';
 
 ReactDOM.render(
-  <App />,
+  <Router>
+    <App />
+  </Router>,
   document.getElementById('root')
 );
